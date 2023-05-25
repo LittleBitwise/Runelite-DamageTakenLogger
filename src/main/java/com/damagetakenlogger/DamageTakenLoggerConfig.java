9c253171540cd758f95dda50b6290b49d47e85a2
LittleBitwise <fuzzy@techie.net>
2023-05-25 14:14:55 +0300

    Added XP accumulator, updated configs. Config needs to be hooked up.

--- src/main/java/com/damagetakenlogger/DamageTakenLoggerConfig.java
+++ src/main/java/com/damagetakenlogger/DamageTakenLoggerConfig.java
@@ -4,16 +4,52 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
+import java.awt.*;
+
 @ConfigGroup("damagetakenlogger")
 public interface DamageTakenLoggerConfig extends Config
 {
 	@ConfigItem(
-		keyName = "loggerColor",
-		name = "Color",
-		description = "The color of the 'damage taken' messages."
+		keyName = "showDamageTaken",
+		name = "Show Damage Taken",
+		description = "Show a notification whenever you take damage.",
+		position = 10
 	)
-	default String loggerColor()
+	default boolean showDamageTaken()
 	{
-		return "255 0 0"; // todo, unused
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "damageTakenColor",
+		name = "Damage Taken",
+		description = "Color for the 'you were hit' messages.",
+		position = 20
+	)
+	default Color damageTakenColor()
+	{
+		return Color.RED;
+	}
+
+	@ConfigItem(
+		keyName = "showExperienceGained",
+		name = "Show XP Gained",
+		description = "Show the accumulated amount of XP gained per kill.",
+		position = 30
+	)
+	default boolean showExperienceGained()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "experienceGainedColor",
+		name = "XP Gained",
+		description = "Color for the 'you gained XP' messages.",
+		position = 40
+	)
+	default Color experienceGainedColor()
+	{
+		return Color.CYAN;
 	}
 }
--- src/main/java/com/damagetakenlogger/DamageTakenLoggerPlugin.java
+++ src/main/java/com/damagetakenlogger/DamageTakenLoggerPlugin.java
@@ -1,22 +1,28 @@
 package com.damagetakenlogger;
 
 import com.google.inject.Provides;
+
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Actor;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
+import net.runelite.api.Skill;
+import net.runelite.api.events.*;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.api.events.HitsplatApplied;
 import net.runelite.client.util.ColorUtil;
 
 import java.awt.*;
+import java.util.Arrays;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Logs the damage you've taken to chat."
+	name = "Damage Taken Logger",
+	description = "Displays damage taken in chat, as well as total XP gained per kill."
 )
 public class DamageTakenLoggerPlugin extends Plugin
 {
@@ -35,6 +41,8 @@ public class DamageTakenLoggerPlugin extends Plugin
 	@Override
 	protected void shutDown() throws Exception
 	{
+		combatActor = null;
+		combatBeginXp = 0;
 		log.info("Damage taken logger stopped!");
 	}
 
@@ -44,13 +52,21 @@ public class DamageTakenLoggerPlugin extends Plugin
 		return configManager.getConfig(DamageTakenLoggerConfig.class);
 	}
 
+
+
+	@Nullable
+	private static Actor combatActor;
+	private static int combatBeginXp;
+
+
+
 	@Subscribe
 	public void onHitsplatApplied(HitsplatApplied hitsplatApplied)
 	{
-		log.debug(String.format("%s was hit for %d",
-				hitsplatApplied.getActor().getName(),
-				hitsplatApplied.getHitsplat().getAmount()
-		));
+//		log.debug(String.format("%s was hit for %d",
+//				hitsplatApplied.getActor().getName(),
+//				hitsplatApplied.getHitsplat().getAmount()
+//		));
 
 		if (hitsplatApplied.getActor() != client.getLocalPlayer()) return;
 
@@ -65,8 +81,63 @@ public class DamageTakenLoggerPlugin extends Plugin
 		this.addGameMessage(message);
 	}
 
+	/**
+	 * Update combat actor when attacking new target.
+	 * Reset time and accumulated XP.
+	 */
+	@Subscribe
+	public void onInteractingChanged(InteractingChanged interact)
+	{
+		if (interact.getSource() != client.getLocalPlayer()) return;
+
+		Actor target = interact.getTarget();
+
+		if (target == null) {
+			log.debug("TODO: HANDLE IT OR REMOVE AS REDUNDANT");
+		} else {
+			log.debug("COMBAT START");
+			combatBeginXp = calculateTotalCombatXp();
+		}
+
+		combatActor = target;
+	}
+
+	/**
+	 * On kill, log accumulated XP.
+	 */
+	@Subscribe
+	public void onActorDeath(ActorDeath ev)
+	{
+		if (ev.getActor() == combatActor) {
+			int combatAfterXp = calculateTotalCombatXp();
+			log.debug("COMBAT SUCCESSFUL");
+
+			String message = ColorUtil.wrapWithColorTag(
+					String.format("Gained %d XP!", combatAfterXp - combatBeginXp),
+					new Color(0, 166, 255) // Todo: get from config
+			);
+			this.addGameMessage(message);
+			combatBeginXp = combatAfterXp; // Just in case.
+			combatActor = null;
+		}
+	}
+
 	private void addGameMessage(String message)
 	{
 		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", message, null);
+	}
+
+	private int calculateTotalCombatXp()
+	{
+		int[] combatSkills = {
+				client.getSkillExperience(Skill.ATTACK),
+				client.getSkillExperience(Skill.DEFENCE),
+				client.getSkillExperience(Skill.HITPOINTS),
+				client.getSkillExperience(Skill.MAGIC),
+				client.getSkillExperience(Skill.RANGED),
+				client.getSkillExperience(Skill.STRENGTH),
+		};
+
+		return Arrays.stream(combatSkills).sum();
 	}
 }
